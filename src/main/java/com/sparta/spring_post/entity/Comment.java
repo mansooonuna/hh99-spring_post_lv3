7f79e95933e040cc2d18cf19f8c8b301011e4806
mansooonuna <parkdasom95@gmail.com>
2023-04-24 18:00:47 +0900

    fix : comment 엔티티에 Post_id 가 외래키로 설정되도록 수정

--- src/main/java/com/sparta/spring_post/entity/Comment.java
+++ src/main/java/com/sparta/spring_post/entity/Comment.java
@@ -15,8 +15,9 @@ public class Comment extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Long postId;
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
     @Column(nullable = false)
     private String content;
@@ -26,10 +27,10 @@ public class Comment extends Timestamped {
     private Users users;
 
 
-    public Comment(Users user, CommentRequestDto commentRequestDto) {
+    public Comment(Users user, CommentRequestDto commentRequestDto, Post post) {
         this.users = user;
         this.content = commentRequestDto.getContent();
-        this.postId = commentRequestDto.getPostId();
+        this.post = post;
     }
 
 
